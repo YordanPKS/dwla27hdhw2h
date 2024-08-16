6135071c72c3ca51417d7e99c458c9fa4bddd96c
YordanPKS <140574806+YordanPKS@users.noreply.github.com>
2024-08-16 01:08:50 -0400

    Update dow.py

--- dow.py
+++ dow.py
@@ -21,7 +21,7 @@ def download_file(client, message):
     url = message.text.split()[1]
     download_folder = 'downloads'  # Replace with your desired download folder
     ydl_opts = {
-    'format_id': 'best[height<=480]',
+    'format': 'best[height<=480]',
     'outtmpl': f'{download_folder}/%(title)s.%(ext)s', # Guarda los videos en la carpeta "downloads"
     'cookiefile': 'cookies.txt'
     }
