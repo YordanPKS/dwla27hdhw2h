69495f330eb114a34a54c24b9a19917aafc39e85
YordanPKS <140574806+YordanPKS@users.noreply.github.com>
2024-08-17 01:08:09 -0400

    Update dow.py

--- dow.py
+++ dow.py
@@ -24,7 +24,7 @@ def download_video_insta(client, message):
     
     # Aquí puedes cargar tu sesión de cookies
     try:
-        loader.load_session_from_file("yordan09prueba")  # Reemplaza "tu_usuario" con tu nombre de usuario
+        loader.load_session_from_file('yordan09prueba', sessionfile='session-yordan09prueba')  # Reemplaza "tu_usuario" con tu nombre de usuario
     except FileNotFoundError:
         app.send_message(message.chat.id, "No se encontró la sesión. Asegúrate de haber iniciado sesión previamente.")
         return
